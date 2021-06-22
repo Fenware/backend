1c4fc0db52f26e0a5d2dfe023af7e139d08618c8
MateConAzucar <fds.workmail@gmail.com>
2021-06-22 19:39:22 -0300

    feat: Gives json

--- src/index.php
+++ src/index.php
@@ -1,10 +1,8 @@
-index
 <?php
 
 include_once 'core/model.php';
 
 $model = new Model();
-echo '<pre>';
-print_r($model->query('SELECT * FROM user'));
-echo '</pre>';
+echo json_encode($model->query('SELECT * FROM user'));
+
 ?>
