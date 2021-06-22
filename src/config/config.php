e78bcdec5e5b821fb79658dc22cbd7b66b70b137
MateConAzucar <fds.workmail@gmail.com>
2021-06-22 13:57:48 -0300

    feat: Basic constants

---
+++ src/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+const URL = "http://localhost:8080/";
+
+const HOST = "mariadb";
+const DATABASE = "database";
+const USER_DB = "admin";
+const PASS_DB = "admin";
+
+const APP_NAME = "Chathink";
+
+const SECRET_KEY = "c6390228-572c-452c-89e8-2e850ef9d357";
