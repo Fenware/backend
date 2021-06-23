d26bce03a154be9d5c5fe60a2836c016cf2773ab
MateConAzucar <fds.workmail@gmail.com>
2021-06-23 18:43:38 -0300

    feat: post,get,delete,put

--- src/api/materia.php
+++ src/api/materia.php
@@ -42,8 +42,9 @@ class MateriaAPI extends API implements iAPI{
                 $postBody = file_get_contents('php://input');
                 $data = json_decode($postBody,true);
                 if(isset($data['nombre'])){
-                    $rows = $this->materia->postMateria($data['nombre']);
-                    if($rows){
+                    $nombre = $data['nombre'];
+                    $rows = $this->materia->postMateria($nombre);
+                    if($rows>=1){
                         http_response_code(200);
                         
                     }else{
@@ -71,16 +72,88 @@ class MateriaAPI extends API implements iAPI{
         }else{
             //Todo correcto/Haga el trabajo
             //TODO
+            if($token->user_type == 'administrador'){
+                //$datosArray = $this->user->getAllUsers();
+                $postBody = file_get_contents('php://input');
+                $data = json_decode($postBody,true);
+                if(isset($data['id'])){
+                    $id = $data['id'];
+                    $datosArray = $this->materia->getMateriaId($id);
+                }elseif(isset($data['nombre'])){
+                    $nombre = $data['nombre'];
+                    $datosArray = $this->materia->getMateriasNombre($nombre);
+                }else{
+                    $datosArray = $this->materia->getMaterias();
+                }
+                echo json_encode($datosArray);
+            }else{
+                echo json_encode($this->res->error('No tienes los permisos para acceder a este recurso'));
+            }
             
         }
     }
 
     public function PUT(){
+        $token = parent::checkToken($this->res);
+        //Chequeo que el token sea valido
+        if (!parent::validToken($token)){
+            //Token invalido
+            header('HTTP/1.1 401 Unauthorized');
+            exit;
+        }else{
+            //Token valido/ Haga el trabajo
+            if($token->user_type == 'administrador'){
+                //$datosArray = $this->user->getAllUsers();
+                $postBody = file_get_contents('php://input');
+                $data = json_decode($postBody,true);
+                if(isset($data['id']) && isset($data['nombre'])){
+                    $id = $data['id'];
+                    $nombre = $data['nombre'];
+                    $rows = $this->materia->putMateria($id,$nombre);
+                    $datosArray = $rows;
+                }else{
+                    $datosArray = $this->res->error_400();
+                }
+                echo json_encode($datosArray);
+            }else{
+                echo json_encode($this->res->error('No tienes los permisos para acceder a este recurso'));
+            }
 
+        }
     }
 
     public function DELETE(){
-
+        $token = parent::checkToken($this->res);
+        //Chequeo que el token sea valido
+        if (!parent::validToken($token)){
+            //Token invalido
+            header('HTTP/1.1 401 Unauthorized');
+            exit;
+        }else{
+            //Todo correcto/Haga el trabajo
+            if($token->user_type == 'administrador'){
+                //$datosArray = $this->user->getAllUsers();
+                $postBody = file_get_contents('php://input');
+                $data = json_decode($postBody,true);
+                if(isset($data['id'])){
+                    $id = $data['id'];
+                    $rows = $this->materia->deleteMateria($id);
+                    if($rows>=1){
+                        http_response_code(200);
+                        
+                    }else{
+                        http_response_code(500);
+                    }
+                    $datosArray = $rows;
+                }else{
+                    $datosArray = $this->res->error_400();
+                }
+                echo json_encode($datosArray);
+            }else{
+                echo json_encode($this->res->error('No tienes los permisos para acceder a este recurso'));
+            }
+            
+        }
     }
 
 }
