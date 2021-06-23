41381076d43f17396ed57868715aeb9d6cfbf36a
MateConAzucar <fds.workmail@gmail.com>
2021-06-23 16:59:02 -0300

    refactor: now auth calls an api object

--- src/auth.php
+++ src/auth.php
@@ -1,23 +1,6 @@
 <?php
 
-include_once 'model/auth.model.php';
-include_once 'core/response.php';
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-$res = new Response();
-$auth = new AuthModel();
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $postBody = file_get_contents('php://input');
-    $datosArray = $auth->login($postBody);
+include_once 'api/auth.php';
 
-    if(isset($datosArray['result']['error_id'])){
-        $response_code = $datosArray['result']['error_id'];
-        http_response_code($response_code);
-    }else{
-        http_response_code(200);
-    }
-    echo json_encode($datosArray);
-}else{
-    $datosArray = $res->error_405();
-    echo json_encode($datosArray);
-}
+$auth = new AuthAPI();
+
