68a269dbf643f90d6a22ec4b9801e61d2438a1ea
lucaspintos909 <lucaspintos909@gmail.com>
2021-06-23 14:11:16 -0300

    refactor: Saque la tabla user_token

--- users.sql
+++ users.sql
@@ -28,19 +28,5 @@ CREATE TABLE administrador(
     FOREIGN KEY(id) REFERENCES user(id)
 );
 
-
-CREATE TABLE user_token (
-  tokenId 				INT PRIMARY KEY NOT NULL,
-  userId 				INT NOT NULL,
-  token 				VARCHAR(45) NOT NULL,
-  estado 				TINYINT(1)  NOT NULL,
-  fecha					DATETIME 	NOT NULL,
-  FOREIGN KEY(userId) REFERENCES user(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
--- desc docente; --
--- select * from administrador; --
--- password : mnoseadmin1234 --
 INSERT INTO user(ci,nombre,apellido,email,avatar,nickname,password,estado_cuenta) values('00000000','Administrador','Administrador','administrador@admin.com','/assets/admin.png','administrador','$2y$10$NOA9YzGzXsE.DCGwMMor2uYcl5ZtJGJxCix88blfVIcNg3H7c7KKW',1);
 INSERT INTO administrador(id) value(1);
