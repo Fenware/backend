78931989261073e3ee5ac4a3cd7c01378f5b437a
MateConAzucar <fds.workmail@gmail.com>
2021-06-27 13:49:25 -0300

    feat: sql complete script

--- users.sql
+++ users.sql
@@ -1,42 +1,139 @@
-/*ENTIDADES:*/
-CREATE TABLE user(
-	id 					INT PRIMARY KEY AUTO_INCREMENT,
-    ci					VARCHAR(8) 	 UNIQUE NOT NULL ,
-    nombre 				VARCHAR(16)  NOT NULL		,
-    segundo_nombre		VARCHAR(16)					,
-    apellido 			VARCHAR(16)	 NOT NULL		,
-    segundo_apellido	VARCHAR(16)					,
-    email				VARCHAR(100) NOT NULL		,
-    avatar 				VARCHAR(30)	 NOT NULL 		,
-    nickname 			VARCHAR(16)  NOT NULL 		,
-    estado_conexion 	TINYINT(1)	 				,
-	password			CHAR(128)	 NOT NULL		,
-    estado_cuenta		TINYINT(1)	 NOT NULL
+CREATE SCHEMA proyecto;
+USE proyecto;
+ALTER DATABASE proyecto CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+/* Entities */
+CREATE TABLE `user`(  
+	id 					INT PRIMARY KEY AUTO_INCREMENT NOT NULL,  
+    ci 					CHAR(8) UNIQUE NOT NULL,
+    `name` 				VARCHAR(16) NOT NULL,
+    middle_name 		VARCHAR(16),
+    surname 			VARCHAR(16) NOT NULL,
+    second_surname 		VARCHAR(16),
+    email 				VARCHAR(100) NOT NULL,
+    avatar 				VARCHAR(50),
+    nickname 			VARCHAR(32),  
+    state_account 		TINYINT(1) NOT NULL DEFAULT 2, -- 0 inactiv 1 activ 2 pendent 
+    `password` 			VARCHAR(128) NOT NULL -- hashed pasword
+); 
+
+CREATE TABLE administrator(
+	id 					INT PRIMARY KEY NOT NULL,
+	FOREIGN KEY(id) REFERENCES `user`(id)
 );
 
-CREATE TABLE alumno(
-	id 					INT PRIMARY KEY,
-    FOREIGN KEY(id) REFERENCES user(id)
+CREATE TABLE teacher(
+	id 					INT PRIMARY KEY NOT NULL,
+    FOREIGN KEY(id) REFERENCES `user`(id)
 );
 
-CREATE TABLE docente(
-	id 					INT PRIMARY KEY,
-    FOREIGN KEY(id) REFERENCES user(id)
+CREATE TABLE student(
+	id 					INT PRIMARY KEY NOT NULL,
+	FOREIGN KEY(id) REFERENCES `user`(id)
 );
 
-CREATE TABLE administrador(
-	id 					INT PRIMARY KEY,
-    FOREIGN KEY(id) REFERENCES user(id)
+CREATE TABLE `subject`(
+	id 					INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` 				VARCHAR(50) NOT NULL,
+    state 				TINYINT(1) NOT NULL DEFAULT 1
 );
 
-create table materia(
-	id 					INT PRIMARY KEY AUTO_INCREMENT,
-	nombre 				VARCHAR(50) NOT NULL /*ADA Web tiene mas de 40 chars*/,
-    estado				TINYINT(1) NOT NULL DEFAULT 1/* 0 inactivo ,1 activo*/
+CREATE TABLE orientation(
+	id 					INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` 				VARCHAR(50) NOT NULL,
+    `year`				INT NOT NULL,
+    state 				TINYINT(1) NOT NULL DEFAULT 1
 );
-ALTER TABLE materia
-ALTER estado SET DEFAULT 1;
-select * from materia;
+
+CREATE TABLE `group`(
+	id 					INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_orientation 		INT NOT NULL,
+	`name` 				VARCHAR(3) UNIQUE NOT NULL,
+	`code` 				CHAR(8) UNIQUE NOT NULL,
+    state 				TINYINT(1) NOT NULL DEFAULT 1,
+    FOREIGN KEY(id_orientation) REFERENCES orientation(id)
+);
+
+CREATE TABLE consult_schedule(
+	id_teacher 			INT NOT NULL,
+	`day` 				TINYINT(1) NOT NULL,
+    start_hour 			TIME NOT NULL,
+    end_hour 			TIME NOT NULL,
+    PRIMARY KEY(id_teacher,`day`),
+    FOREIGN KEY(id_teacher) REFERENCES teacher(id)
+);
+
+CREATE TABLE `query`(
+	id 					INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_student 			INT NOT NULL,
+    id_teacher 			INT NOT NULL,
+    id_group 			INT NOT NULL,
+    id_subject 			INT NOT NULL,
+    theme 				VARCHAR(50) NOT NULL,
+    creation_date		DATETIME NOT NULL,
+    finish_date 		DATETIME,
+    `resume`			TEXT,
+    state 				TINYINT(1) NOT NULL DEFAULT 1, /*1 realizada por el alumno 2 contestada por el  3 recibida*/
+    FOREIGN KEY(id_student) REFERENCES student(id),
+    FOREIGN KEY(id_teacher, id_group, id_subject) REFERENCES teacher_group_subject(id_teacher, id_group, id_subject)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE room(
+	id 					INT PRIMARY KEY NOT NULL,
+    FOREIGN KEY(id) REFERENCES `query`(id)
+);
+
+CREATE TABLE individual(
+	id 					INT PRIMARY KEY NOT NULL,
+    FOREIGN KEY(id) REFERENCES `query`(id)
+);
+
+CREATE TABLE message(
+	id 					INT AUTO_INCREMENT NOT NULL,
+    id_query 			INT NOT NULL,
+    id_user				INT NOT NULL,
+    content 			TEXT NOT NULL,
+    `date` 				DATETIME NOT NULL,
+    PRIMARY KEY(id, id_query),
+    FOREIGN KEY(id_user) REFERENCES user(id),
+    FOREIGN KEY(id_query) REFERENCES `query`(id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+/* Relations */
+
+CREATE TABLE subject_orientation(
+	id_subject 			INT NOT NULL,
+    id_orientation 		INT NOT NULL,
+	PRIMARY KEY(id_subject, id_orientation),
+    FOREIGN KEY(id_subject) REFERENCES `subject`(id),
+	FOREIGN KEY(id_orientation) REFERENCES orientation(id)
+);
+
+CREATE TABLE teacher_group(
+	id_teacher 			INT NOT NULL,
+    id_group 			INT NOT NULL,
+	PRIMARY KEY(id_teacher, id_group),
+    FOREIGN KEY(id_teacher) REFERENCES teacher(id),
+	FOREIGN KEY(id_group) REFERENCES `group`(id)
+);
+
+
+CREATE TABLE teacher_group_subject(
+	id_teacher 			INT NOT NULL,
+    id_group 			INT NOT NULL,
+    id_subject			INT NOT NULL,
+	PRIMARY KEY(id_teacher, id_group,id_subject),
+    FOREIGN KEY(id_teacher,id_group) REFERENCES teacher_group(id_teacher, id_group)
+);
+
+CREATE TABLE student_group(
+	id_student			INT NOT NULL,
+    id_group 			INT NOT NULL,
+	PRIMARY KEY(id_students, id_group),
+    FOREIGN KEY(id_student) REFERENCES student(id),
+	FOREIGN KEY(id_group) REFERENCES `group`(id)
+);
+
+
 
 INSERT INTO user(ci,nombre,apellido,email,avatar,nickname,password,estado_cuenta) values('00000000','Administrador','Administrador','administrador@admin.com','/assets/admin.png','administrador','$2y$10$NOA9YzGzXsE.DCGwMMor2uYcl5ZtJGJxCix88blfVIcNg3H7c7KKW',1);
 INSERT INTO administrador(id) value(1);
