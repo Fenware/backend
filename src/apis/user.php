4e7f7e4d823afe48e8e50919700d456b79ede2c2
MateConAzucar <fds.workmail@gmail.com>
2021-07-02 19:52:32 -0300

    feat: post method  so  admin  can create new users

--- src/apis/user.php
+++ src/apis/user.php
@@ -16,7 +16,158 @@ class UserAPI extends API{
     }
 
     public function POST($token,$data){
-        //TODO
+        if($token->user_type == 'administrator'){
+            if(!$this->isDataCorrect($data)){
+                $datosArray = $this->res->error_400();
+            }else{ 
+                $exists = $this->userExists($data);
+                if($exists == false){
+                    $id = $this->user->postUser($data);
+                    if($id != 'error'){
+                        $type = $data['type'];
+                        $type = $this->filterType($type);
+                        if($type != 'error'){
+                            $this->insertOptionalData($id,$data);
+                            $datosArray = $this->user->setUserType($id,$type);
+                            $datosArray = $this->user->patchUser($id,'state_account',1);
+                        }else{
+                            $datosArray = $this->res->error_400();
+                        }
+                    }else{
+                        $datosArray = $this->res->OOPSIE();
+                    }
+                }else{
+                    $datosArray = $exists;
+                }
+            }
+            echo json_encode($datosArray);
+        }else{
+            echo json_encode($this->res->error_403());
+        }
+    }
+
+    private function userExists($data){ 
+        $array = $this->user->getUser($data['email']);
+        if(count($array) > 0){
+            return $this->res->error('Este email ya esta tomado');
+        }else{
+            $array = $this->user->getUser($data['nickname']);
+            if(count($array) > 0){
+                return $this->res->error('Este nickname ya esta tomado');
+            }else{
+                $array = $this->user->getUserByCi($data['ci']);
+                if(count($array) > 0){
+                    return $this->res->error('Esta cedula ya esta tomada');
+                }else{
+                    return false;
+                }
+            }
+            
+        }
+    }
+    private function insertOptionalData($id,$data){
+        if(isset($data['middle_name'])){
+            $this->user->patchUser($id,'middle_name',$data['middle_name']);
+        }
+
+        if(isset($data['second_surname'])){
+            $this->user->patchUser($id,'second_surname',$data['second_surname']);
+        }
+
+        if(isset($data['avatar'])){
+            $this->user->patchUser($id,'avatar',$data['avatar']);
+        }
+    }
+    private function filterType($type){
+        switch($type){
+            //admin  no vo por que nadie se puede autoregistrar como admin
+            case 'teacher':
+                $type = 'teacher';
+                break;
+            case 'student':
+                $type = 'student';
+                break;
+            default:
+                $type = 'error';
+                break;
+        }
+        return $type;
+    }
+
+    private function isObligatoryDataCorrect($data){
+        if(    !isset($data['ci'])
+            || strlen($data['ci']) != 8
+            || empty($data['ci'])
+            || !isset($data['name']) 
+            || empty($data['name'])
+            || !isset($data['surname']) 
+            || empty($data['surname'])
+            || !isset($data['email']) 
+            || empty($data['email'])
+            || !isset($data['nickname'])
+            || empty($data['nickname'])
+            || !isset($data['password'])
+            || empty($data['password'])
+            || !isset($data['type'])){
+            //No existen
+            return false;
+        }else
+            //Existen los campos obligatorios
+            //Los compos estan  correctos?
+            if(    !is_string($data['ci'])
+                || !is_string($data['name'])
+                || !is_string($data['surname'])
+                || !$this->is_email($data['email'])
+                || !is_string($data['nickname'])
+                || !is_string($data['password'])
+                || !is_string($data['type'])){
+                    //No son correctos
+                    return false;
+                }else{
+                    //Son correctos
+                    return true;
+                }
+    }
+
+    private function isOptionalDataCorrect($data){
+        $status = true;
+        //Cada chequeo es si se intento enviar infromacion opcional y en ese caso si la informacion esta bien
+        if(isset($data['middle_name']) && !is_string($data['middle_name'])){
+            $status = false;
+        }
+
+        if(isset($data['second_surname']) && !is_string($data['second_surname'])){
+            $status = false;
+        }
+
+        if(isset($data['avatar']) && !is_string($data['avatar'])){
+            $status = false;
+        }
+
+        return $status;
+    }
+    private function isDataCorrect($data){
+        if(!$this->isObligatoryDataCorrect($data) || !$this->isOptionalDataCorrect($data)){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    private function setUserType($id,$type){
+        if($type == 'teacher'){
+
+        }elseif($type == 'student'){
+
+        }
+    }
+
+    private function is_email($email){
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public function GET($token,$data){
