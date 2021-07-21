df491a50222eb6c34e37cda51c065df507ac09b3
MateConAzucar <fds.workmail@gmail.com>
2021-07-20 22:33:32 -0300

    fix:removed distinct

--- src/model/subject.model.php
+++ src/model/subject.model.php
@@ -97,7 +97,7 @@ class SubjectModel extends Model{
     }
     
     public function getTeacherSubjects($teacher){
-        $stm = 'SELECT tgs.id_group,tgs.id_orientation, DISTINCT tgs.id_subject ,s.name
+        $stm = 'SELECT tgs.id_group,tgs.id_orientation,tgs.id_subject ,s.name
         FROM s `subject` , tgs teacher_group_subject
         WHERE tgs.id_teacher = ? AND s.id = tgs.id_subject AND tgs.state = 1 AND s.state = 1';
         $data = parent::query($stm,[$teacher]);
