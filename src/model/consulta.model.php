7a30192bd30806ec4937819fe0b56db0fbcae791
MateConAzucar <fds.workmail@gmail.com>
2021-07-24 20:37:59 -0300

    feat: post and  get from consultas return more data

--- src/model/consulta.model.php
+++ src/model/consulta.model.php
@@ -30,9 +30,22 @@ class ConsultaModel extends Model{
         $teacher = parent::query($stm_teacher,[$consulta[0]['id_teacher']]);
         //agrego el campo teacher_name
         $consulta[0]['teacher_name'] = $teacher[0]['name'].' '.$teacher[0]['surname'];
+
+        //busco el nombre de la materia
+        $stm = 'SELECT * FROM `subject` WHERE id = ?';
+        $subject = parent::query($stm,[$id_subject]);
+        //agrego el campo subject_name
+        $consulta[0]['subject_name'] = $subject[0]['name'];
+
+        //busco el grupo
+        $stm = 'SELECT * FROM `group` WHERE id = ?';
+        $group = parent::query($stm,[$id_group]);
+        //agrego el campo subject_name
+        $consulta[0]['group_name'] = $group[0]['name'];
         return $consulta;
     }
 
+  
     public function getConsultasFromUser($id){
         $stm = 'SELECT * FROM `query` WHERE id_student = ? OR id_teacher = ? AND `state` != 0';
         $consultas = parent::query($stm,[$id,$id]);
@@ -47,9 +60,24 @@ class ConsultaModel extends Model{
             $teacher = parent::query($stm_teacher,[$consulta['id_teacher']]);
             //agrego el campo teacher_name
             $consulta['teacher_name'] = $teacher[0]['name'].' '.$teacher[0]['surname'];
+            //busco el nombre de la materia
+            $stm = 'SELECT * FROM `subject` WHERE id = ?';
+            $id_subject = $consulta['id_subject'];
+            $subject = parent::query($stm,[$id_subject]);
+            //agrego el campo subject_name
+            $consulta['subject_name'] = $subject[0]['name'];
+
+            //busco el grupo
+            $stm = 'SELECT * FROM `group` WHERE id = ?';
+            $id_group = $consulta['id_group'];
+            $group = parent::query($stm,[$id_group]);
+            //agrego el campo subject_name
+            $consulta['group_name'] = $group[0]['name'];
         }
         return $consultas;
     }   
+
+    
 
     public function postMessagge($user,$consulta,$content){
         $stm = 'INSERT INTO `message`(id_user,id_query,content,`date`) VALUES(?,?,?,?)';
