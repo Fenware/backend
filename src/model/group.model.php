db4f44d37a866b5ef3fc90c368bd9ef83dd6695e
MateConAzucar <fds.workmail@gmail.com>
2021-08-30 21:28:07 -0300

    feat: setting and getting conection_time

--- src/model/group.model.php
+++ src/model/group.model.php
@@ -159,7 +159,7 @@ class GroupModel extends Model{
     }
 
     public function getStudentsInGroup($group){
-        $stm = 'SELECT u.id,ci,`name`,middle_name,surname,second_surname,email,avatar,nickname,state_account
+        $stm = 'SELECT u.id,ci,`name`,middle_name,surname,second_surname,email,avatar,nickname,state_account,connection_time
         FROM `user` u,student_group sg
         WHERE u.id = sg.id_student AND sg.id_group = ?';
         $users = parent::query($stm , [$group]);
@@ -167,7 +167,7 @@ class GroupModel extends Model{
     }
 
     public function getTeachersInGroup($group){
-        $stm = 'SELECT u.id,ci,`name`,middle_name,surname,second_surname,email,avatar,nickname,state_account
+        $stm = 'SELECT u.id,ci,`name`,middle_name,surname,second_surname,email,avatar,nickname,state_account,connection_time
         FROM `user` u,teacher_group tg
         WHERE u.id = tg.id_student AND tg.id_group = ?';
         $teachers = parent::query($stm , [$group]);
