19c5215b84d4747592496f4286d5accd6b8dc55b
MateConAzucar <fds.workmail@gmail.com>
2021-09-02 00:13:01 -0300

    fix:arreglando  querys

--- src/model/chat.model.php
+++ src/model/chat.model.php
@@ -125,13 +125,13 @@ class ChatModel extends QueryModel{
         switch($type){
             case 'teacher':
                 $stm = 'SELECT q.id ,q.id_student,q.id_teacher,q.id_group,q.id_subject,q.theme ,q.creation_date, q.finish_date,q.`resume`,q.`state`
-                FROM `query` q,`room` r,group g,teacher_group sg
-                WHERE q.`state` != 0 AND sg.id_teacher = ? AND sg.id_group = g.id AND q.id = r.id AND g.id = q.id_group';
+                FROM `query` q,`room` r
+                WHERE q.`state` != 0 AND q.id = r.id AND q.id_teacher = ?';
                 break;
             case 'student':
                 $stm = 'SELECT q.id ,q.id_student,q.id_teacher,q.id_group,q.id_subject,q.theme ,q.creation_date, q.finish_date,q.`resume`,q.`state`
-                FROM `query` q,`room` r,group g,student_group sg
-                WHERE q.`state` != 0 AND sg.id_student = ? AND sg.id_group = g.id AND q.id = r.id AND g.id = q.id_group';
+                FROM `query` q,`room` r,`group` g,student_group sg
+                WHERE q.`state` != 0 AND q.id = r.id AND sg.id_group = q.id_group AND sg.id_student = ? AND g.id_group';
                 break;
         }
         $consultas = parent::query($stm,[$id]);
