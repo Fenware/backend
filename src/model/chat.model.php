cd3c34109a0949f06d64641c83d936bc56d50a17
MateConAzucar <fds.workmail@gmail.com>
2021-09-09 17:19:36 -0300

    fix: fixed query

--- src/model/chat.model.php
+++ src/model/chat.model.php
@@ -173,7 +173,7 @@ class ChatModel extends QueryModel{
 
     public function getParticipants($chat){
         $stm = 'SELECT r.id ,u.name,u.middle_name,u.surname,u.second_surname
-        room_participants r,`user` u
+        FROM room_participants r,`user` u
         WHERE r.id_room = ? AND r.id_user = u.id';
         $participants = parent::query($stm , [$chat] );
         return $participants;
