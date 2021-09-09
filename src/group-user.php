b5639bc12cdc6b74ac834b3ce0884697e162c7e3
MateConAzucar <fds.workmail@gmail.com>
2021-09-08 21:33:14 -0300

    removing un necesary files

--- src/group-user.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include_once 'apis/group-user.php';
-
-$uGroup = new GroupUserAPI();
--- src/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-include_once $_SERVER['DOCUMENT_ROOT'].'/core/response.php';
-$res = new Response();
-echo json_encode($res->error('There is nothing in here'));
